07321509568bd4d8891eba7765cb67986aa983e6
Geoff Winsor <glwinsor@sfu.ca>
2018-06-01 10:09:59 -0700

    Update arguments

--- ivclient.py
+++ ivclient.py
@@ -10,46 +10,37 @@ from optparse import OptionParser
 
 # Get options from the command line
 parser = OptionParser()
-parser.add_option("-a", "--accession", dest="accession", action="store", type="string", help="Accession is the RefSeq accession for the desired reference genome used in the contig reordering step (mandatory for draft genomes)", metavar="ACCESSION")
-parser.add_option("-t", "--token", dest="token", action="store", type="string", help="Your authentication token for the API. This is found under'JOBS' > 'HTTP API Token' when you login at www.pathogenomics.sfu.ca/islandviewer and is valid for 30 days before having to be refreshed.", metavar="TOKEN")
-parser.add_option("-g", "--genbank", dest="genbank", action="store", type="string", help="Name of the genbank file used for input to IslandViewer.", metavar="GENBANK")
-parser.add_option("-i", "--indir", dest="indir", action="store", type="string", help="The directory containing the genbank file used for input.", metavar="INDIR")
-parser.add_option("-o", "--outdir", dest="outdir", action="store", type="string", help="The directory to output IslandViewer results to.", metavar="OUTDIR")
+parser.add_option("--accession", dest="accession", action="store", type="string", help="Accession is the RefSeq accession for the desired reference genome used in the contig reordering step (mandatory for draft genomes)", metavar="ACCESSION")
+parser.add_option("--token", dest="token", action="store", type="string", help="Your authentication token for the API. This is found under'JOBS' > 'HTTP API Token' when you login at www.pathogenomics.sfu.ca/islandviewer and is valid for 30 days before having to be refreshed.", metavar="TOKEN")
+parser.add_option("--gbk", dest="gbk", action="store", type="string", help="Provide a name for the reordered genbank file containing genomic islands predicted by IslandViewer (output file).", metavar="GENBANK")
+parser.add_option("--seq","--sequence", dest="sequence", action="store", type="string", help="Provide a name for the sequence file (genbank format) used as input.", metavar="SEQUENCE")
+parser.add_option("--tab", dest="tab", action="store", type="string", help="Provide a name for the tab-delimited file that summarizes predicted genomic islands (output file).", metavar="TAB")
 
 (options, args) = parser.parse_args()
 
 if options.accession == None:
     parser.error("Missing argument for --accession ")    
-if options.indir == None:
-    parser.error("Missing argument for --indir ") 
-if options.outdir == None:
-    parser.error("Missing argument for --outdir ") 
 if options.token == None:
     parser.error("Missing argument for --token ")       
-if options.genbank == None:
-    parser.error("Missing argument for --genbank ")    
+if options.gbk == None:
+    parser.error("Missing argument for --gbk ")    
+if options.sequence == None:
+    parser.error("Missing argument for --sequence ") 
+if options.tab == None:
+    parser.error("Missing argument for --tab ") 
 
-if not os.path.isdir(options.indir):
-    sys.stderr.write("Could not find input directory: "+options.indir+"\n")
-    sys.exit()
 
-if not os.path.isdir(options.outdir):
-    sys.stderr.write("Could not find output directory: "+options.outdir+"\n")
-    sys.exit()
-
-if not os.path.isfile(options.indir+"/"+options.genbank):
-    sys.stderr.write("Could not find file '"+options.genbank+"' in input directory '"+ options.indir + "'\n")
+if not os.path.isfile(options.sequence):
+    sys.stderr.write("Could not find file '"+options.sequence+"'\n")
     sys.exit()
     
 host = "http://www.pathogenomics.sfu.ca/islandviewer"
 
 
-
-
 multipart_data = MultipartEncoder(
     fields={ "format_type": "GENBANK",
              'ref_accnum': options.accession,
-             'genome_file': ('filename', open(options.indir+"/"+options.genbank, 'rb'), 'text/plain')}
+             'genome_file': ('filename', open(options.sequence, 'rb'), 'text/plain')}
 )
 headers={'Content-Type': multipart_data.content_type,'x-authtoken': options.token}
   
@@ -68,8 +59,6 @@ else:
     sys.stderr.write("Error submitting genome to IslandViewer"+"\n")
     sys.exit()
     
-# Strip extension from the genbank file name
-samplename=re.sub("\.\w+$","",options.genbank)
 
 headers={'Content-Type': "text/plain",'x-authtoken': options.token} 
 
@@ -80,15 +69,13 @@ while job_status !='Complete':
     decoded = response.json()
     job_status=decoded['status']
     time.sleep(30)
-    print(job_status+"\n")
      
 # Download the Genbank including genomic island predictions, and the reordered concatenated contigs
 request_status=""
 while request_status !=200:
-    response = requests.get(host+"/rest/job/"+job_token+"/download/genbank/", headers=headers, stream=True)
-   
+    response = requests.get(host+"/rest/job/"+job_token+"/download/genbank/", headers=headers, stream=True)   
     request_status=response.status_code
-    local_filename=options.outdir+"/"+samplename+"_islandviewer.gbk"
+    local_filename=options.gbk
     with open(local_filename, 'wb') as f:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk: 
@@ -98,10 +85,9 @@ while request_status !=200:
 # Download the table summarizing genomic island predictions
 request_status=""
 while request_status !=200:
-    response = requests.get(host+"/rest/job/"+job_token+"/download/tab/", headers=headers, stream=True)
-   
+    response = requests.get(host+"/rest/job/"+job_token+"/download/tab/", headers=headers, stream=True)   
     request_status=response.status_code
-    local_filename=options.outdir+"/"+samplename+"_islandviewer.tbl"
+    local_filename=options.tab
     with open(local_filename, 'wb') as f:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk: 
